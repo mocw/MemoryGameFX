fa7e28dc18f5721b1f98f4fab47b910863e6d3a2
wojmoc <wojmoc96@gmail.com>
2020-05-24 05:13:54 +0200

    wybieranie zdjec

--- src/lodz/uni/math/GameController.java
+++ src/lodz/uni/math/GameController.java
@@ -1,33 +1,82 @@
 package lodz.uni.math;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.GridPane;
-
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 
 public class GameController implements Initializable {
 
     @FXML
     private GridPane gridPane;
-    private List<ToggleButton> images = new ArrayList<>();
+    private List<ToggleImage> images = new ArrayList<>();
     private boolean isBusy = false;
+    private List<Integer> drawnNumbers = new ArrayList<>();
+    private List<byte[]> byteImages = new ArrayList<>();
+    private Random random = new Random();
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        try {
+            drawImages();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int imageIndex = 0;
         for(int i=0; i<5; i++){
             for(int j=0; j<4; j++){
-                ToggleImage toggleImage = new ToggleImage();
+                ToggleImage toggleImage = new ToggleImage(new Image(byteImages.get(imageIndex)));
+                imageIndex++;
                 toggleImage.setMaxHeight(Double.MAX_VALUE);
                 toggleImage.setMaxWidth(Double.MAX_VALUE);
+                toggleImage.setOnAction(new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(ActionEvent event) {
+                        try {
+                            toggleImage.reverse();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
                 gridPane.add(toggleImage, i, j , 1, 1);
             }
         }
     }
+
+    private void drawImages() throws IOException {
+        int numbers = 0;
+        while(true){
+            int randomNumber = random.nextInt(15 + 1 - 1) + 1;
+            if(!drawnNumbers.contains(randomNumber)){
+                numbers++;
+                drawnNumbers.add(randomNumber);
+            }
+            if(numbers == 10) break;
+        }
+        for(Integer i : drawnNumbers){
+            BufferedImage bImage = ImageIO.read(new File("./images/pic" + i + ".png"));
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ImageIO.write(bImage, "png", bos );
+            byte [] image = bos.toByteArray();
+            byteImages.add(image);
+            byteImages.add(image);
+        }
+        Collections.shuffle(byteImages);
+    }
+
 }
--- src/lodz/uni/math/ToggleImage.java
+++ src/lodz/uni/math/ToggleImage.java
@@ -1,20 +1,27 @@
 package lodz.uni.math;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Node;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 
 public class ToggleImage extends ToggleButton {
 
     private Image front;
+    private boolean isClicked = false;
+    private boolean isMatching = false;
 
-    public ToggleImage() {
+    public ToggleImage(Image image) {
         super();
+        this.front = image;
     }
-
-//    public ToggleImage(Image image) {
-//        super();
-//        this.front = image;
-//    }
 
     public ToggleImage(String s, Image image) {
         super(s);
@@ -26,6 +33,25 @@ public class ToggleImage extends ToggleButton {
         this.front = image;
     }
 
+    public void reverse() throws IOException  {
+        if(isMatching) {
+            return;
+        }
+        if(isClicked) {
+//            setBackground(back);
+            isClicked = false;
+        } else {
+            ByteArrayInputStream bis = new ByteArrayInputStream(this.front.getImage());
+            BufferedImage bImage2 = ImageIO.read(bis);
+            javafx.scene.image.Image image = SwingFXUtils.toFXImage(bImage2, null);
+
+            BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+            Background background = new Background(backgroundImage);
+
+            this.setBackground(background);
+            isClicked = true;
+        }
+    }
 
 
 }
